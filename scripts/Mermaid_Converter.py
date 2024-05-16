2429d571e95278505472381193082cb45917f946
matze2402 <145361419+matze2402@users.noreply.github.com>
2024-05-16 10:29:23 +0200

    Update Mermaid_Converter.py

--- scripts/Mermaid_Converter.py
+++ scripts/Mermaid_Converter.py
@@ -1,70 +1,77 @@
 import os
+import csv
 import pandas as pd
-
-# Path to the directory containing the CSV files (relative to the repository root)
-csv_directory = "./CSV_Files"
-
-# Initialize the dictionary to store CSV file data
 dict_of_csv_files = {}
+for i in os.listdir("CSV_Files"):
+    i_1 = i.replace(" ", ":").replace(".csv", "")
+    with open(f"CSV_Files/{i}", 'r') as file:
+        # Create a CSV reader object
+        csv_file = pd.DataFrame(pd.read_csv(f"CSV_Files/{i}",delimiter=','))
+        test = csv_file.T.to_dict(orient='list')
+        test_2 = {}
+        if i_1 == "dcat:Resource":
+            test_2.update({'arrow': {"mandatory": []}})
+            test_2['arrow'].update({"recommended": []})
+            test_2['arrow'].update({"optional": []})
+            test_2.update({'regular': {"mandatory": []}})
+            test_2['regular'].update({"recommended": []})
+            test_2['regular'].update({"optional": []})
+        else:
+            test_3 = []
+            test_4 = []
+            test_5 = []
+            test_6 = []
+            test_7 = []
+            test_8 = []
+            for i_2 in test:
+                if test[i_2][4] == "-":
+                    if test[i_2][2] == "mandatory":
+                        test_3.append(f"{test[i_2][1]} {test[i_2][5]} [{test[i_2][2].replace('mandatory', '1').replace('recommended', '0').replace('optional', '1')}...{'*'if test[i_2][6] else '1'}] ")
+                    elif test[i_2][2] == "recommended":
+                        test_4.append(f"{test[i_2][1]} {test[i_2][5]} [{test[i_2][2].replace('mandatory', '1').replace('recommended', '0').replace('optional', '1')}...{'*'if test[i_2][6] else '1'}] ")
+                    elif test[i_2][2] == "optional":
+                        test_5.append(f"{test[i_2][1]} {test[i_2][5]} [{test[i_2][2].replace('mandatory', '1').replace('recommended', '0').replace('optional', '1')}...{'*'if test[i_2][6] else '1'}] ")
+                elif test[i_2][3] == "-":
+                    if test[i_2][2] == "mandatory":
+                        test_6.append(f"{test[i_2][1]} {test[i_2][5]} [{test[i_2][2].replace('mandatory', '1').replace('recommended', '0').replace('optional', '1')}...{'*'if test[i_2][6] else '1'}] ")
+                    elif test[i_2][2] == "recommended":
+                        test_7.append(f"{test[i_2][1]} {test[i_2][5]} [{test[i_2][2].replace('mandatory', '1').replace('recommended', '0').replace('optional', '1')}...{'*'if test[i_2][6] else '1'}] ")
+                    elif test[i_2][2] == "optional":
+                        test_8.append(f"{test[i_2][1]} {test[i_2][5]} [{test[i_2][2].replace('mandatory', '1').replace('recommended', '0').replace('optional', '1')}...{'*'if test[i_2][6] else '1'}] ")
+            test_2.update({'arrow': {"mandatory": test_3}})
+            test_2['arrow'].update({"recommended": test_4})
+            test_2['arrow'].update({"optional": test_5})
+            test_2.update({'regular': {"mandatory": test_6}})
+            test_2['regular'].update({"recommended": test_7})
+            test_2['regular'].update({"optional": test_8})
 
-# Iterate through each file in the specified directory
-for filename in os.listdir(csv_directory):
-    if filename.endswith(".csv"):
-        file_key = filename.replace(" ", ":").replace(".csv", "")
-        
-        # Read the CSV file into a pandas DataFrame
-        csv_path = os.path.join(csv_directory, filename)
-        csv_file = pd.read_csv(csv_path, delimiter=',')
-        csv_dict = csv_file.T.to_dict(orient='list')
-        
-        # Initialize the structure for file_data
-        file_data = {'arrow': {"mandatory": [], "recommended": [], "optional": []},
-                     'regular': {"mandatory": [], "recommended": [], "optional": []}}
-            
-        # Process each row in the CSV file
-        for key, values in csv_dict.items():
-            attr_type = values[2]
-            arrow_type = values[4] == "-"
-            regular_type = values[3] == "-"
-            attr_desc = f"{values[1]} {values[5]} [{attr_type.replace('mandatory', '1').replace('recommended', '0').replace('optional', '1')}...{'*' if values[6] else '1'}]"
 
-            if arrow_type:
-                file_data['arrow'][attr_type].append(attr_desc)
-            elif regular_type:
-                file_data['regular'][attr_type].append(attr_desc)
-        
-        dict_of_csv_files[file_key] = file_data
+        dict_of_csv_files.update({i_1: test_2})
 
-# Prepare the data for Mermaid diagrams
-mermaid_data = "```mermaid\nclassDiagram\n"
-dict_of_csv_files_for_mermaid = {}
+        test_14 = ''
 
-for class_name, attributes in dict_of_csv_files.items():
-    class_block = f'class `{class_name}` {{\n'
-    for category, attrs in attributes['regular'].items():
-        class_block += f'  # {category}\n'
-        for attr in attrs:
-            class_block += f'  {attr.replace(":", "#58;")}\n'
-    class_block += '}\n'
-    mermaid_data += class_block
-    dict_of_csv_files_for_mermaid[class_name] = class_block
+        dict_of_csv_files_for_mermaid = {}
+        for i_4 in dict_of_csv_files:
+            test_9 = 'class `' + i_4 +'` {\n'
+            for i_5 in dict_of_csv_files[i_4]['regular']:
+                test_9 = test_9 + '  # ' + i_5 + '\n'
+                for i_6 in dict_of_csv_files[i_4]['regular'][i_5]:
+                    test_9 = test_9 + '  ' + i_6.replace(':','#58;') + '\n'
+            test_9 = test_9 + '}' + '\n'
+            test_14 += test_9
+            dict_of_csv_files_for_mermaid.update({i_4: test_9})
 
-# Generate arrow relationships for Mermaid
-for class_name, attributes in dict_of_csv_files.items():
-    arrow_relationships = ""
-    for category, attrs in attributes['arrow'].items():
-        for attr in attrs:
-            source_class = f'`{class_name}`'
-            target_class = f'`{attr.split()[1]}`'
-            attribute_name = attr.split()[0].replace(':', '#58;')
-            relationship_type = attr.split()[2].replace('[', '"').replace(']', '"').replace('...', '..')
-            arrow_relationships += f'{source_class} --> {relationship_type} {target_class} : {attribute_name}\n'
-    mermaid_data += arrow_relationships
+        for i_6 in dict_of_csv_files:
+            test_10 = ''
+            for i_7 in dict_of_csv_files[i_6]['arrow']:
+                for i_8 in dict_of_csv_files[i_6]['arrow'][i_7]:
+                    test_11_1 = '`' + i_6 + '`'
+                    test_11 = '`' + i_8.split()[1] + '`'
+                    test_12 = i_8.split()[0].replace(':','#58;')
+                    test_13 = i_8.split()[2].replace('[','"').replace(']','"').replace('...','..')
+                    test_10 = test_10 + f'{test_11_1} --> {test_13} {test_11} : {test_12}' + '\n'
+            test_14 += test_10 + '\n'
 
-mermaid_data += "```\n"
-
-# Append Mermaid formatted data to README.md
-readme_path = 'README.md'
-with open(readme_path, 'a') as readme_file:
-    readme_file.write('\n## Generated Mermaid Diagram\n')
-    readme_file.write(mermaid_data)
+    test_15 = '```mermaid \nclassDiagram\n\n' + test_14 + '\n```'
+with open(f"Mermaid_Diagramm.md", 'w') as file:
+    file.write(test_15)
